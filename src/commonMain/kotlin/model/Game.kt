1a8ed2c6d50aa6ee6f759792fd393e540228a4c2
humza <ahmadhumza7@gmail.com>
2023-05-13 14:12:32 -0700

    Added the entire SF6 Roster.

--- src/commonMain/kotlin/model/Game.kt
+++ src/commonMain/kotlin/model/Game.kt
@@ -12,7 +12,22 @@ enum class Game(val gameName: String, val abbr: String, val roster: List<Charact
 }
 
 val STREETFIGHTER6CAST = listOf(
-    Character(name = "Ryu"),
-    Character(name = "Chun Li"),
+    Character(name = "Cammy"),
+    Character(name = "Lily"),
+    Character(name = "Zangief"),
+    Character(name = "JP"),
+    Character(name = "Marisa"),
+    Character(name = "Manon"),
+    Character(name = "Dee Jay"),
+    Character(name = "E. Honda"),
+    Character(name = "Dhalsim"),
+    Character(name = "Blanka"),
     Character(name = "Ken"),
+    Character(name = "Juri"),
+    Character(name = "Kimberly"),
+    Character(name = "Guile"),
+    Character(name = "Chun Li"),
+    Character(name = "Jamie"),
+    Character(name = "Luke"),
+    Character(name = "Ryu"),
 )
